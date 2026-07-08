82ff7dbfbdbda3004c9197e765cd9ce282f5bdb9
이동호 <ehdgh0115@gmail.com>
2026-07-08 17:30:14 +0900

    Feat: 임대료 조회 기능 추가

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,3 +1,3 @@
-from app.routers import admin, businesses, commercial, forecast, population, sales
+from app.routers import admin, analysis, businesses, commercial, forecast, population, sales
 
-__all__ = ["commercial", "population", "businesses", "sales", "forecast", "admin"]
+__all__ = ["commercial", "analysis", "population", "businesses", "sales", "forecast", "admin"]
